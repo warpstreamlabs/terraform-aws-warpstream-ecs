5cc057fe61faec652d140384bd71ca095f0b1c55
Brian Shih <brianshihstart@gmail.com>
2025-08-12 14:02:23 -0700

    Tweak

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v683"
+  default     = "v684"
 }
 
 variable "warpstream_virtual_cluster_id" {
