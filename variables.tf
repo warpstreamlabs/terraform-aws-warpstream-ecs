09d7f4a576fbbe461f8f94d3b9cf9f90c6431760
Maud Gautier <maud.j.gautier@gmail.com>
2025-08-26 13:41:14 -0600

    doc: release v693

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v692"
+  default     = "v693"
 }
 
 variable "warpstream_virtual_cluster_id" {
