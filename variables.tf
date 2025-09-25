088f32ff16af3f98178fc4e3532d69af39d44ee0
Towerthousand <dani.torramilans@gmail.com>
2025-09-25 11:57:25 +0200

    v707 ECS

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v706"
+  default     = "v707"
 }
 
 variable "warpstream_virtual_cluster_id" {
