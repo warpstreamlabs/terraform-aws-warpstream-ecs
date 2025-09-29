41c9155074fd98eceeb22a5a4cd1208e603d9e66
Brian Shih <brianshihstart@gmail.com>
2025-09-29 11:41:19 -0700

    708

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v707"
+  default     = "v708"
 }
 
 variable "warpstream_virtual_cluster_id" {
