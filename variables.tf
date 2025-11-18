79fe289a95e90f62a695b18a3f1c43622ac9ac2e
Sami Tabet <salph.tabet@gmail.com>
2025-11-18 11:04:09 +0100

    tf-aws: update agent to v726

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v725"
+  default     = "v726"
 }
 
 variable "warpstream_virtual_cluster_id" {
