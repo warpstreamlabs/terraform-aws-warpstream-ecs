bfea975ca716a518c7ec46a687715528edad2641
Aratz Lasa <alasa@confluent.io>
2026-04-28 15:48:03 -0600

    doc: release v787

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v786"
+  default     = "v787"
 }
 
 variable "warpstream_virtual_cluster_id" {
