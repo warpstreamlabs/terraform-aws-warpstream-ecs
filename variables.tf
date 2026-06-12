775ae801353a00c474f794721b79b52083ffb11b
Brian Shih <brianshihstart@gmail.com>
2026-06-12 07:27:10 -0700

    doc: release v807

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v806"
+  default     = "v807"
 }
 
 variable "warpstream_virtual_cluster_id" {
