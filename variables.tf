78da9728da45dd3d2ffb767810b68f86121aad51
Brian Shih <brianshihstart@gmail.com>
2026-07-29 15:10:04 -0700

    doc: release v826

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "kafka_port" {
 variable "warpstream_agent_version" {
   description = "The version of the warpstream agent to deploy"
   type        = string
-  default     = "v825"
+  default     = "v826"
 }
 
 variable "warpstream_virtual_cluster_id" {
